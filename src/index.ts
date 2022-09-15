5edd273e73fe2b336a488b28c569833262bab0ce
devin jhu <devin.jhu@mths.ca>
2022-09-15 13:47:43 +0000

    code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!")
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * By:      Devin Jhu
+ * Version: 1.0
+ * Since:   2020-01-01
+ */
+console.log('Hello, World!')
+console.log('\ndone.')
